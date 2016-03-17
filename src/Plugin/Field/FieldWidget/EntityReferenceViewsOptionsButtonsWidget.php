3cc17b66f021c760bb777d20810164d09f2d5f0e
Eliot  Scott <eliotwsc@malt.umdl.umich.edu>
2016-03-17 14:45:13 -0400

    a module to use Entity Reference Views as select lists, checkboxes or radio buttons in forms

---
+++ src/Plugin/Field/FieldWidget/EntityReferenceViewsOptionsButtonsWidget.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\entity_reference_views_select\Plugin\Field\FieldWidget\EntityReferenceViewsOptionsButtonsWidget.
+ */
+
+namespace Drupal\entity_reference_views_select\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsWidgetBase;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Views;
+
+/**
+ * Plugin implementation of the 'erviews_options_buttons' widget.
+ *
+ * @FieldWidget(
+ *   id = "erviews_options_buttons",
+ *   label = @Translation("Entity Reference Views Check boxes/radio buttons"),
+ *   field_types = {
+ *     "entity_reference",
+ *   },
+ *   multiple_values = TRUE
+ * )
+ */
+class EntityReferenceViewsOptionsButtonsWidget extends OptionsWidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element = parent::formElement($items, $delta, $element, $form, $form_state);
+    $options = $this->getOptions($items->getEntity());
+    $selected = $this->getSelectedOptions($items);
+    if ($this->getFieldSettings()['handler'] == 'views') {
+      $view = Views::getView($this->getFieldSettings()['handler_settings']['view']['view_name']);
+      $view->execute($this->getFieldSettings()['handler_settings']['view']['display_name']);
+      foreach ($view->result as $row) {
+        $options[$row->_entity->id()] = $options[$row->_entity->id()]->create(\Drupal::service('renderer')->render($view->style_plugin->view->rowPlugin->render($row)));
+      }
+    }
+    // If required and there is one single option, preselect it.
+    if ($this->required && count($options) == 1) {
+      reset($options);
+      $selected = array(key($options));
+    }
+
+    if ($this->multiple) {
+      $element += array(
+        '#type' => 'checkboxes',
+        '#default_value' => $selected,
+        '#options' => $options,
+      );
+    }
+    else {
+      $element += array(
+        '#type' => 'radios',
+        // Radio buttons need a scalar value. Take the first default value, or
+        // default to NULL so that the form element is properly recognized as
+        // not having a default value.
+        '#default_value' => $selected ? reset($selected) : NULL,
+        '#options' => $options,
+      );
+    }
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEmptyLabel() {
+    if (!$this->required && !$this->multiple) {
+      return t('N/A');
+    }
+  }
+
+}
---
+++ src/Plugin/Field/FieldWidget/EntityReferenceViewsOptionsSelectWidget.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\entity_reference_views_select\Plugin\Field\FieldWidget\EntityReferenceViewsOptionsSelectWidget.
+ */
+
+namespace Drupal\entity_reference_views_select\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsWidgetBase;
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Views;
+
+/**
+ * Plugin implementation of the 'erviews_options_select' widget.
+ *
+ * @FieldWidget(
+ *   id = "erviews_options_select",
+ *   label = @Translation("Entity Reference Views Select list"),
+ *   field_types = {
+ *     "entity_reference",
+ *   },
+ *   multiple_values = TRUE
+ * )
+ */
+class EntityReferenceViewsOptionsSelectWidget extends OptionsWidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element = parent::formElement($items, $delta, $element, $form, $form_state);
+
+    $options = $this->getOptions($items->getEntity());
+    $selected = $this->getSelectedOptions($items, $delta);
+    if ($this->getFieldSettings()['handler'] == 'views') {
+      $view = Views::getView($this->getFieldSettings()['handler_settings']['view']['view_name']);
+      $view->execute($this->getFieldSettings()['handler_settings']['view']['display_name']);
+      foreach ($view->result as $row) {
+        $options[$row->_entity->id()] = \Drupal::service('renderer')->render($view->style_plugin->view->rowPlugin->render($row));
+      }
+    }
+
+    $element += array(
+      '#type' => 'select',
+      '#options' => $options,
+      '#default_value' => $selected,
+      // Do not display a 'multiple' select box if there is only one option.
+      '#multiple' => $this->multiple && count($this->options) > 1,
+    );
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function sanitizeLabel(&$label) {
+    // Select form inputs allow unencoded HTML entities, but no HTML tags.
+    $label = Html::decodeEntities(strip_tags($label));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function supportsGroups() {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEmptyLabel() {
+    if ($this->multiple) {
+      // Multiple select: add a 'none' option for non-required fields.
+      if (!$this->required) {
+        return t('- None -');
+      }
+    }
+    else {
+      // Single select: add a 'none' option for non-required fields,
+      // and a 'select a value' option for required fields that do not come
+      // with a value selected.
+      if (!$this->required) {
+        return t('- None -');
+      }
+      if (!$this->has_value) {
+        return t('- Select a value -');
+      }
+    }
+  }
+
+}
